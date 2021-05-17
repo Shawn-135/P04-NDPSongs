0cfb45a041f22835d9e43425b053809145ad868f
Michell <19030685@myrp.edu.sg>
2021-05-17 15:43:43 +0800

    Added Song.java Updated activity_main.xml

--- app/src/main/java/com/myapplicationdev/android/p04_ndpsongs/Song.java
+++ app/src/main/java/com/myapplicationdev/android/p04_ndpsongs/Song.java
@@ -47,7 +47,5 @@ public class Song implements Serializable {
 
     public int getStars() { return stars; }
 
-    public void setStars(int stars) {
-        this.stars = stars;
-    }
+    public void setStars(int stars) { this.stars = stars; }
 }
